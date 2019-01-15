cf656ad6f928db4aa3d4b0440f63dbd4e679d653
Jaideep Prasad <jddeep003@gmail.com>
2019-01-15 15:27:47 +0530

    Sensible names to hardcoded dimens done (#26)

--- app/src/main/java/io/neurolab/MainActivity.java
+++ app/src/main/java/io/neurolab/MainActivity.java
@@ -4,9 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,6 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import io.neurolab.settings.FeedbackSettings;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -71,6 +73,9 @@ public class MainActivity extends AppCompatActivity
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             startActivity(new Intent(MainActivity.this, NeuroSettingsActivity.class));
+            return true;
+        } else if (id == R.id.action_feedback_settings) {
+            startActivity(new Intent(MainActivity.this, FeedbackSettings.class));
             return true;
         }
 
