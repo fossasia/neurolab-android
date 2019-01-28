ef72fef3e112a6f95ea2bd665868e9964eab056d
jd003walker <41433222+jd003walker@users.noreply.github.com>
2019-01-28 18:41:31 +0530

    Removed FAB and Hello world text

--- app/src/main/java/io/neurolab/MainActivity.java
+++ app/src/main/java/io/neurolab/MainActivity.java
@@ -13,7 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import io.neurolab.settings.FeedbackSettings;
 
@@ -26,15 +25,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
