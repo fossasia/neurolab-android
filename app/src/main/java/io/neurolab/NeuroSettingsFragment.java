e69d2c427cdaec0cca10e2d972aad262a1ed1096
jd003walker <41433222+jd003walker@users.noreply.github.com>
2019-02-12 14:20:54 +0530

    Made variable for dependency

--- app/src/main/java/io/neurolab/NeuroSettingsFragment.java
+++ app/src/main/java/io/neurolab/NeuroSettingsFragment.java
@@ -3,28 +3,31 @@ package io.neurolab;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompat;
+import android.support.annotation.Nullable;
+import android.support.v7.preference.EditTextPreference;
 import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
+import android.widget.EditText;
 
 public class NeuroSettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener, Preference.OnPreferenceChangeListener {
     Context context = getContext();
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     public NeuroSettingsFragment() {
 
     }
 
     @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+    public void onCreatePreferencesFix(@Nullable Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.neuro_settings_fragment);
         Preference preference = findPreference(getResources().getString(R.string.samples_pref_key));
         preference.setOnPreferenceChangeListener(this);
+        sharedPreferences = getPreferenceScreen().getSharedPreferences();
 
-        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
         PreferenceScreen preferenceScreen = getPreferenceScreen();
         int count = preferenceScreen.getPreferenceCount();
-
-
     }
 
     @Override
@@ -35,13 +38,13 @@ public class NeuroSettingsFragment extends PreferenceFragmentCompat implements S
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
     }
 
     @Override
