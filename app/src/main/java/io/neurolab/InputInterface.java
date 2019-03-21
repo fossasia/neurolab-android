50e555cadea4462a4d7947bd4927dea0e172f71b
Rohit Jain <rohitjain3241@gmail.com>
2019-03-21 20:45:36 +0530

    Added the interfaces InputInterface and DataReceiver (#81)

---
+++ app/src/main/java/io/neurolab/InputInterface.java
@@ -0,0 +1,12 @@
+package io.neurolab;
+
+public interface InputInterface extends Runnable {
+
+    public int shutDown();
+    public boolean isConnectionSuccessful();
+    public boolean sendCommand(String string);
+    public boolean record(String filename);
+    public void stopRecording();
+    public boolean isRecording();
+
+}
