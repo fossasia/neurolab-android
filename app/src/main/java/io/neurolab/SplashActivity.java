8caa6609cee5eafe8b88df33d4d87172266f9039
Vishal Sehgal <codervishalsehgal@gmail.com>
2019-03-29 14:44:29 +0530

    Added SplashActivity and Layouts

---
+++ app/src/main/java/io/neurolab/SplashActivity.java
@@ -0,0 +1,13 @@
+package io.neurolab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
