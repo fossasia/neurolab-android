4fa264dc1850078aaf88a4c44298392df9c5a02c
Jaideep Prasad <jddeep003@gmail.com>
2019-04-07 01:36:19 +0530

    feat: Implement AboutUs Section Fragment (#125)

---
+++ app/src/main/java/io/neurolab/AboutUsActivity.java
@@ -0,0 +1,13 @@
+package io.neurolab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about__us);
+    }
+}
