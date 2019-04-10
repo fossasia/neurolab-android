63f5dc98438704ad61b856ae2fa06a8cceb01cdc
Vishal Sehgal <codervishalsehgal@gmail.com>
2019-04-11 01:55:00 +0530

    feat: Implemented Relax Screen (#142)

---
+++ app/src/main/java/io/neurolab/fragments/FocusVisualFragment.java
@@ -0,0 +1,30 @@
+package io.neurolab.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.neurolab.R;
+import io.neurolab.visuals.SpaceAnimationVisuals;
+
+public class FocusVisualFragment extends android.support.v4.app.Fragment {
+
+    private int lastPos = 0;
+    private int newPos = -300;
+    private boolean moving = false;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_focus_visual, container, false);
+
+        SpaceAnimationVisuals.moveRocket(view.findViewById(R.id.rocketimage), lastPos, newPos, moving);
+
+        return view;
+    }
+
+}
---
+++ app/src/main/java/io/neurolab/fragments/RelaxVisualFragment.java
@@ -0,0 +1,27 @@
+package io.neurolab.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.neurolab.R;
+import io.neurolab.tools.Animations;
+
+public class RelaxVisualFragment extends android.support.v4.app.Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_relax_visual, container, false);
+
+        Animations.rotateView(view.findViewById(R.id.yantraOneImageView), 360f, 0);
+        Animations.rotateView(view.findViewById(R.id.yantraTwoImageView), 0, 360f);
+
+        return view;
+    }
+
+}
