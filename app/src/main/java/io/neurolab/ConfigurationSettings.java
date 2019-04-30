82034e146654c567ca5859feb47aa26d03369b56
Suresh Gandhi <sureshgandhi200@gmail.com>
2019-04-30 13:48:09 +0530

    Added the NFB Relax Configuration Settings (#167)

--- app/src/main/java/io/neurolab/ConfigurationSettings.java
+++ app/src/main/java/io/neurolab/ConfigurationSettings.java
@@ -3,8 +3,14 @@ package io.neurolab;
 public class ConfigurationSettings {
 
     private ServerSettings serverSettings = new ServerSettings();
+    private NFBRelaxSettings nfbRelaxSettings = new NFBRelaxSettings();
 
     public ServerSettings getServerSettings() {
         return serverSettings;
     }
+
+    public NFBRelaxSettings getNfbRelaxSettings(){
+        return nfbRelaxSettings;
+    }
+
 }
