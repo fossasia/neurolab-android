3d1a523ec3dcfa8d33abfa1c95bc8516e3189804
Suresh Gandhi <sureshgandhi200@gmail.com>
2019-04-30 13:45:58 +0530

    refactor: Refactored the incorrect naming of about us activity layout file (#169)

--- app/src/main/java/io/neurolab/About_Us.java
+++ app/src/main/java/io/neurolab/About_Us.java
@@ -8,6 +8,6 @@ public class About_Us extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about__us);
+        setContentView(R.layout.activity_about_us);
     }
 }
