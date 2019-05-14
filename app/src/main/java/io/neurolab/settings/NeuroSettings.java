b91fe1f337db067d56e64d2a80971e3e2fb79c8d
Jaideep Prasad <jddeep003@gmail.com>
2019-05-14 10:49:31 +0530

    Implemented FFTPreprocessor (#189)

--- app/src/main/java/io/neurolab/settings/NeuroSettings.java
+++ app/src/main/java/io/neurolab/settings/NeuroSettings.java
@@ -1,12 +1,28 @@
 package io.neurolab.settings;
 
 
+import android.annotation.TargetApi;
+import android.os.Build;
+
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
 public class NeuroSettings {
 
     //Default place holder values
     private int samplesPerSecond = 3;
     private int bins = 4;
     private int numChannels = 2;
+
+    public ConcurrentLinkedDeque<double[]> getCurrentData() {
+        return currentData;
+    }
+
+    public void setCurrentData(ConcurrentLinkedDeque<double[]> currentData) {
+        this.currentData = currentData;
+    }
+
+    protected ConcurrentLinkedDeque<double[]> currentData = new ConcurrentLinkedDeque<double[]>();
 
     // Constructors
     public NeuroSettings() {
