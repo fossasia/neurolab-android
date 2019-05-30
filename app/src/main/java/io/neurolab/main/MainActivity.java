cd800a0e095ca6f46da21f297018abcb0d8cbe71
Jaideep Prasad <jddeep003@gmail.com>
2019-05-30 10:44:56 +0530

    Implemented Test Mode Interface for UART testing (#255)

--- app/src/main/java/io/neurolab/main/MainActivity.java
+++ app/src/main/java/io/neurolab/main/MainActivity.java
@@ -150,6 +150,8 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
+        } else if(id == R.id.nav_test) {
+            startActivity(new Intent(this, TestModeActivity.class));
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
