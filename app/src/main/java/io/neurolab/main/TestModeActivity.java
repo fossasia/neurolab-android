582a2e661ed208bfe6f3e817278deeb7383ae029
Suresh Gandhi <sureshgandhi200@gmail.com>
2019-06-08 13:29:31 +0530

    Android Arduino bridge testing done (#274)

--- app/src/main/java/io/neurolab/main/TestModeActivity.java
+++ app/src/main/java/io/neurolab/main/TestModeActivity.java
@@ -1,5 +1,6 @@
 package io.neurolab.main;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,7 +10,6 @@ import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -23,13 +23,20 @@ import com.felhr.usbserial.UsbSerialDevice;
 import com.felhr.usbserial.UsbSerialInterface;
 
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 import io.neurolab.R;
 
 public class TestModeActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
     private DeviceConnector deviceConnector;
+
+    private Button beginBtn;
     private Button readBtn;
     private Button writeBtn;
+    private Button stopBtn;
+
     private EditText editText;
     private TextView displayView;
     private Spinner baudRateSpinner;
@@ -39,17 +46,17 @@ public class TestModeActivity extends AppCompatActivity implements AdapterView.O
     private UsbSerialDevice serialPort;
     private UsbDeviceConnection connection;
     private int baudRate = 9600;
+    private static final int ARDUINO_DEVICE_ID = 0x2341;
 
     private final String ACTION_USB_PERMISSION = "io.neurolab.USB_PERMISSION";
 
-    private UsbSerialInterface.UsbReadCallback callBack = new UsbSerialInterface.UsbReadCallback() { //Defining a Callback which triggers whenever data is read.
+    private UsbSerialInterface.UsbReadCallback readCallback = new UsbSerialInterface.UsbReadCallback() { //Defining a Callback which triggers whenever data is read.
         @Override
         public void onReceivedData(byte[] arg0) {
             String data = null;
             try {
                 data = new String(arg0, "UTF-8");
-                data.concat("/n");
-                setReadText(displayView, data);
+                updateView(displayView, data);
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
@@ -59,10 +66,7 @@ public class TestModeActivity extends AppCompatActivity implements AdapterView.O
     private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() { //Broadcast Receiver to automatically start and stop the Serial connection.
         @Override
         public void onReceive(Context context, Intent intent) {
-            switch (intent.getAction()) {
-                case UsbManager.ACTION_USB_DEVICE_ATTACHED:
-                    deviceConnector.checkConnection(context);
-                    break;
+            switch (intent.getAction()){
                 case ACTION_USB_PERMISSION:
                     boolean granted =
                             intent.getExtras().getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED);
@@ -71,15 +75,15 @@ public class TestModeActivity extends AppCompatActivity implements AdapterView.O
                         serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection);
                         if (serialPort != null) {
                             if (serialPort.open()) { //Set Serial Connection Parameters.
-                                writeBtn.setEnabled(true);
+                                setUiEnabled(true); //Enable Buttons in UI
                                 serialPort.setBaudRate(baudRate);
                                 serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
                                 serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
                                 serialPort.setParity(UsbSerialInterface.PARITY_NONE);
                                 serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
-                                serialPort.read(callBack);
-                                setReadText(displayView, getResources()
-                                        .getString(R.string.connection_opened) + "\n");
+                                serialPort.read(readCallback); //
+                                updateView(displayView, "Serial Connection Opened!\n");
+
                             } else {
                                 Log.d("SERIAL", "PORT NOT OPEN");
                             }
@@ -91,52 +95,91 @@ public class TestModeActivity extends AppCompatActivity implements AdapterView.O
                     }
                     break;
                 default:
-                    deviceConnector.checkConnection(context);
                     break;
             }
-        }
+        };
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_mode);
+
+        // Setting listeners of all the three buttons
+        beginBtn = findViewById(R.id.buttonBegin);
+        readBtn = findViewById(R.id.buttonRead);
+        writeBtn = findViewById(R.id.buttonWrite);
+        stopBtn = findViewById(R.id.buttonStop);
+
+        // to start reading from the Arduino.
+        editText = findViewById(R.id.editText);
+        // to display the data received through the serial port.
+        displayView = findViewById(R.id.displayData);
+        // to choose a baud rate.
+
+        beginBtn.setOnClickListener(v -> searchForArduinoDevice());
+        writeBtn.setOnClickListener(v -> write(v));
+        stopBtn.setOnClickListener(v -> closeConnection());
+
+        baudRateSpinner = findViewById(R.id.baud_rates_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.baud_rates_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        baudRateSpinner.setAdapter(adapter);
+        baudRateSpinner.setOnItemSelectedListener(this);
+
         // setting up the UsbManager instance with the desired USB service.
         usbManager = (UsbManager) getSystemService(this.USB_SERVICE);
         // initializing the DeviceConnector instance for checking and connecting to the device.
         deviceConnector = new DeviceConnector(usbManager);
-        // to start reading from the Arduino.
-        readBtn = findViewById(R.id.buttonRead);
-        // to send and write data to the Arduino.
-        writeBtn = findViewById(R.id.buttonWrite);
-        // to input the data to be sent.
-        editText = findViewById(R.id.editText);
-        // to display the data sent and received through the serial port.
-        displayView = findViewById(R.id.displayData);
-        // to choose a baud rate.
-        baudRateSpinner = findViewById(R.id.baud_rates_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.baud_rates_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // setting the adapter with the spinner.
-        baudRateSpinner.setAdapter(adapter);
-        baudRateSpinner.setOnItemSelectedListener(this);
-        writeBtn.setEnabled(false);
+
         IntentFilter intentFilter = new IntentFilter();
         // adding the possible USB intent actions.
         intentFilter.addAction(ACTION_USB_PERMISSION);
-        intentFilter.addAction(UsbManager.ACTION_USB_ACCESSORY_ATTACHED);
-        intentFilter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
         registerReceiver(broadcastReceiver, intentFilter);
     }
 
-    /**
-     * To start the connection and reading from the Arduino.
-     *
-     * @param view
+    /*
+        Searches for all connected devices and then check if the vendor ID of the Arduino
+        matches that of a connected device.
+        If found, permission must be requested from the user.
      */
-    public void beginRead(View view) {
-        deviceConnector.checkConnection(this);
-        device = deviceConnector.getDevice();
+    private void searchForArduinoDevice(){
+        HashMap usbDevices = usbManager.getDeviceList();
+
+        if (!usbDevices.isEmpty()) {
+            boolean keep = true;
+            for (Object object : usbDevices.entrySet()) {
+                Map.Entry<String, UsbDevice> entry = (Map.Entry<String, UsbDevice>) object;
+                device = entry.getValue();
+
+                int deviceVID = device.getVendorId();
+                if (deviceVID == ARDUINO_DEVICE_ID){                   //Arduino Vendor ID = 0x2341
+                    PendingIntent pi = PendingIntent.getBroadcast(this, 0,
+                            new Intent(ACTION_USB_PERMISSION), 0);
+                    usbManager.requestPermission(device, pi);
+                    keep = false;
+                } else {
+                    connection = null;
+                    device = null;
+                }
+
+                if (!keep)
+                    break;
+            }
+        }
+    }
+
+    // For closing the serial connection
+    private void closeConnection(){
+        serialPort.close();
+        setUiEnabled(false);
+    }
+
+    // Sets UI enabled or disabled as per the state passed in
+    private void setUiEnabled(boolean state){
+        readBtn.setEnabled(state);
+        writeBtn.setEnabled(state);
+        stopBtn.setEnabled(state);
     }
 
     /**
@@ -144,24 +187,22 @@ public class TestModeActivity extends AppCompatActivity implements AdapterView.O
      *
      * @param view
      */
-    public void beginWrite(View view) {
+    public void write(View view) {
         String string = editText.getText().toString();
         serialPort.write(string.getBytes());
-        setReadText(displayView, "\n" + getResources().getString(R.string.data_sent) +
-                string + "\n");
+        editText.setText("");
     }
 
     /**
-     * To set the display text with the data being received from the Arduino through the serial
+     * To append the textview tv with the string text.
      * port.
      *
      * @param tv
      * @param text
      */
-    private void setReadText(TextView tv, String text) {
+    private void updateView(TextView tv, String text) {
         final TextView displayView = tv;
         final String ftext = text;
-
         runOnUiThread(() -> displayView.append(ftext));
     }
 
