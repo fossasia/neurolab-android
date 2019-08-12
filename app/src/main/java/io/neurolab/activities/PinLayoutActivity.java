884d366665f23d1bee8300d6f511f0a8c250fbd3
jddeep <jddeep003@gmail.com>
2019-08-12 11:25:58 +0530

    Zoom/Drag feature enabled in PIN layout screen

--- app/src/main/java/io/neurolab/activities/PinLayoutActivity.java
+++ app/src/main/java/io/neurolab/activities/PinLayoutActivity.java
@@ -1,23 +1,106 @@
 package io.neurolab.activities;
 
+import android.annotation.SuppressLint;
+import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 
 import io.neurolab.R;
 
-public class PinLayoutActivity extends AppCompatActivity {
+public class PinLayoutActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    private Matrix matrix = new Matrix();
+    private Matrix savedMatrix = new Matrix();
+
+    private static final int NONE = 0;
+    private static final int DRAG = 1;
+    private static final int ZOOM = 2;
+    private int mode = NONE;
+
+    private PointF start = new PointF();
+    private PointF mid = new PointF();
+    private float oldDist = 1f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_pin_layout);
 
-        boolean front = getIntent().getBooleanExtra("layout", true);
+        boolean frontSide = getIntent().getBooleanExtra("layout", true);
         ImageView pinImageView = findViewById(R.id.pin_lay_img_view);
-        if(front)
-            pinImageView.setImageDrawable(getDrawable(R.drawable.front_pin_layout));
-        else
-            pinImageView.setImageDrawable(getDrawable(R.drawable.back_pin_layout));
+        pinImageView.setImageDrawable(frontSide ? getDrawable(R.drawable.front_pin_layout) : getDrawable(R.drawable.back_pin_layout));
+
+        pinImageView.setOnTouchListener(this);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        ImageView view = (ImageView) v;
+        view.setScaleType(ImageView.ScaleType.MATRIX);
+        float scale;
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                matrix.set(view.getImageMatrix());
+                savedMatrix.set(matrix);
+                start.set(event.getX(), event.getY());
+                mode = DRAG;
+                break;
+
+            case MotionEvent.ACTION_POINTER_DOWN:
+                oldDist = spacing(event);
+                if (oldDist > 5f) {
+                    savedMatrix.set(matrix);
+                    midPoint(mid, event);
+                    mode = ZOOM;
+                }
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                if (mode == DRAG) {
+                    matrix.set(savedMatrix);
+                    matrix.postTranslate(event.getX() - start.x, event.getY() - start.y);
+                } else if (mode == ZOOM) {
+                    float newDist = spacing(event);
+                    if (newDist > 5f) {
+                        matrix.set(savedMatrix);
+                        scale = newDist / oldDist;
+                        matrix.postScale(scale, scale, mid.x, mid.y);
+                    }
+                }
+                break;
+
+            default:
+                break;
+        }
+        view.setImageMatrix(matrix);
+
+        return true;
+    }
+
+    private float spacing(MotionEvent event) {
+        float x = 0;
+        float y = 0;
+        try {
+            x = event.getX(0) - event.getX(1);
+            y = event.getY(0) - event.getY(1);
+        } catch (Exception e) {/**/}
+        return (float) Math.sqrt(x * x + y * y);
+    }
+
+    private void midPoint(PointF point, MotionEvent event) {
+        float x = 0;
+        float y = 0;
+        try {
+            x = event.getX(0) + event.getX(1);
+            y = event.getY(0) + event.getY(1);
+        } catch (Exception e) {/**/}
+        point.set(x / 2, y / 2);
     }
 }
