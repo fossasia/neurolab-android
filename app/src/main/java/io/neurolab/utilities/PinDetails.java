dbbf7113ca0825da4771a9e0cab0b93e9b4b9ad0
jddeep <jddeep003@gmail.com>
2019-08-26 18:11:37 +0530

    Pin descriptions enabled with dialog

---
+++ app/src/main/java/io/neurolab/utilities/PinDetails.java
@@ -0,0 +1,32 @@
+package io.neurolab.utilities;
+
+public class PinDetails {
+
+    private String name;
+    private String description;
+    private int categoryColor;
+    private int colorID;
+
+    public PinDetails(String name, String description, int categoryColor, int colorID) {
+        this.name = name;
+        this.description = description;
+        this.categoryColor = categoryColor;
+        this.colorID = colorID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCategoryColor() {
+        return categoryColor;
+    }
+
+    public int getColorID() {
+        return colorID;
+    }
+}
