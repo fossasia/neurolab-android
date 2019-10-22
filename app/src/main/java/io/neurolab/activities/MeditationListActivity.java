179a81ead428535a1acc57fc3356cd7165b73fe3
Shantnu Kumar <me.shantnu@gmail.com>
2019-10-22 11:06:15 +0530

    fix : Removing csv files from meditations list.
    Removed comments.
    
    fix : follow up with Codacy.
    
    fix : Removing CSV files from meditations list.

--- app/src/main/java/io/neurolab/activities/MeditationListActivity.java
+++ app/src/main/java/io/neurolab/activities/MeditationListActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import io.neurolab.R;
 import io.neurolab.adapters.MeditationListAdapter;
@@ -17,14 +22,36 @@ public class MeditationListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_meditation_list);
         setTitle("Meditations");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ArrayList<Field> resFields;
+        ArrayList<Field> finalList;
+        Field[] finalFields;
+        int resId;
+        resFields = new ArrayList<>(Arrays.asList(R.raw.class.getFields()));
+        finalList = new ArrayList<>();
+        for (int z = 0; z < resFields.size(); z++) {
+            String name = resFields.get(z).getName();
+            TypedValue value = new TypedValue();
+            resId = getResources().getIdentifier(name, "raw", getPackageName());
+            getResources().getValue(resId, value, true);
 
+            if ((getExtension(value.string.toString()).indexOf(".csv") == -1)) {
+                finalList.add(resFields.get(z));
+            }
+        }
+
+        finalFields = finalList.toArray(new Field[finalList.size()]);
         RecyclerView meditationsRecyclerView = findViewById(R.id.meditation_recycler_view);
-
-        MeditationListAdapter meditationListAdapter = new MeditationListAdapter(this, R.raw.class.getFields());
+        MeditationListAdapter meditationListAdapter = new MeditationListAdapter(this, finalFields);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(
                 this, LinearLayoutManager.VERTICAL, false);
         meditationsRecyclerView.setLayoutManager(linearLayoutManager);
         meditationsRecyclerView.setAdapter(meditationListAdapter);
+    }
+
+    public String getExtension(String path) {
+        String[] token = path.split("/raw/", 2);
+        String ext = token[1];
+        return ext;
     }
 
     @Override
@@ -33,7 +60,7 @@ public class MeditationListActivity extends AppCompatActivity {
         return true;
     }
 
-     public void onBackPressed() {
+    public void onBackPressed() {
         super.onBackPressed();
         startActivity(new Intent(this, MeditationHome.class));
         finish();
